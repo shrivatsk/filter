44f3bfdceff390ef2a8d17a7f6635b64ba16f685
Kannan <email>
2019-06-17 10:14:11 -0400

    fixed filtering

--- Sift_PP.R
+++ Sift_PP.R
@@ -10,23 +10,16 @@ print("input maf read")
 df <- as.data.frame(input)
 print("input maf converted to df")
 rm(input)
-#filter only keep pass
+#pass filter
 passfilter <- df[grep("pass", df$FILTER, ignore.case = T),]
-print("pass filter done")
 rm(df)
-#check against threshold
-thresholdfilter <- passfilter[apply((passfilter[,124:132]<= threshold) | (is.na(passfilter[124:132])), 1, all),]
-print("threshold filter done")
-#rm(passfilter)
 genes <- read.delim(input_genes, header = F, sep = "", stringsAsFactors = F)
-print("genes read from txt")
 #check against genes
-genefilter <- thresholdfilter[is.element(thresholdfilter$Hugo_Symbol,genes$V1), ]
-print("gene filter done")
-#keep nonsynonymous 
+genefilter <- passfilter[is.element(passfilter$Hugo_Symbol,genes$V1), ]
 mfilter <- genefilter[grep("Frame_Shift_Del|Frame_Shift_Ins|In_Frame_Del|In_Frame_Ins|Missense_Mutation|Nonsense_Mutation|Nonstop_Mutation|Splice_Region|Splice_Site|Targeted_Region|Translation_Start_Site",
-                         genefilter$Variant_Classification, ignore.case = T),]
-print("nonsynonymous filter done")
+                           genefilter$Variant_Classification, ignore.case = T),]
+#check against threshold
+thresholdfilter <- mfilter[apply((mfilter[,124:132] <= threshold) | (is.na(mfilter[,124:132])), 1, all),]
 #keep SIFT predictions
 #siftfilter <- mfilter[grep("tolerated", mfilter$SIFT, ignore.case = TRUE, invert = TRUE),]
 #print("sift filter done")
@@ -37,5 +30,5 @@ print("nonsynonymous filter done")
 #dupl <- rownames(siftfilter) %in% rownames(ppfilter)
 #filtered <- rbind(siftfilter, ppfilter[!dupl,])
 #print("sift and polyphen combined")
-write.table(mfilter, output_maf, sep = "\t", quote = F, row.names = F)
+write.table(thresholdfilter, output_maf, sep = "\t", quote = F, row.names = F)
 print("written to output file")
